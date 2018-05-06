0ba8cc1e1a51289e330ffd5b949e0151c71ee340
Cody Horman <chorman@gustavus.edu>
2018-05-06 15:26:13 -0500

    My changes Other classes.

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/database/OtherDbSchema.java
@@ -0,0 +1,14 @@
+package com.bignerdranch.android.criminalintent.database;
+
+public class OtherDbSchema {
+
+    public static final class OtherTable {
+        public static final String NAME = "Other";
+
+        public static final class Cols {
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String DATE = "date";
+        }
+    }
+}
