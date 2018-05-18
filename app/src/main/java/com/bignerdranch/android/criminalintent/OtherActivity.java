1000caf98b765bb4d2d31c7f488ccc92a83f9f00
Michael <mscheie@gustavus.edu>
2018-05-18 09:42:50 -0500

    Fixed layout for Other

--- app/src/main/java/com/bignerdranch/android/criminalintent/OtherActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/OtherActivity.java
@@ -8,6 +8,6 @@ public class OtherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_other);
+        setContentView(R.layout.fragment_other);
     }
 }
